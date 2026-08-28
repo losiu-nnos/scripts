32a67f661c66fa8e86bc702040337b85fa1a3fdf
TheUnoIunno <pepsiandcolaarethesamething@gmail.com>
2026-08-28 10:07:24 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,5 +5,5 @@ elseif game.PlaceId == 116495829188952 then
     game.Players.LocalPlayer:Kick("script won't work in the lobby, enter a game") -- ded rails lobby
 
 else
-    game.Players.LocalPlayer:Kick("game not supported")
+    game.Players.LocalPlayer:Kick("game not supported yet")
 end
